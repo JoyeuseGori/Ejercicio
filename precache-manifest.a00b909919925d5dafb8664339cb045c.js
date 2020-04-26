2880f74ddef4c50543d0b814746e9ac3c87a5fed
JoyeuseGori <joyeuse.gori@gmail.com>
2020-04-26 14:20:59 -0500

    Updates

---
+++ precache-manifest.a00b909919925d5dafb8664339cb045c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6c2df674e52a4b8d374e08a7a67a18",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "bc5a993278f82aee45c6",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a9ca311f68f84521e7d0",
+    "url": "/react-gh-pages/static/js/2.1d2b3ee7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.1d2b3ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5a993278f82aee45c6",
+    "url": "/react-gh-pages/static/js/main.97f30ea9.chunk.js"
+  },
+  {
+    "revision": "0e914c37fd45acd744d7",
+    "url": "/react-gh-pages/static/js/runtime-main.d4b1385c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
